ecc5c771bf0e485136990244cae44059dbf2c421
Kalpanab87 <kalpana7.ece@gmail.com>
2019-12-08 23:09:18 +0000

    Java project can have many Mains

---
+++ src/learnProgramming/Main.java
@@ -0,0 +1,13 @@
+package learnProgramming;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person myPerson1 = new Person();
+        Person myPerson2 = new Person();
+
+        System.out.println("I am in Main Class");
+    }
+}
---
+++ src/learnProgramming/Person.java
@@ -0,0 +1,4 @@
+package learnProgramming;
+
+public class Person {
+}
